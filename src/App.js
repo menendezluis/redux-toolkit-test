879ddf1fd617c08b4b3c74f7358fdfbbe38bbdf5
Luis Menendez <lmenendez.1@gmail.com>
2021-11-07 10:32:01 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import UserList from './components/UserList';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <UserList />
+    </>
+  );
+}
+
+export default App;
---
+++ src/store/slices/users/index.js
@@ -0,0 +1,13 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+export const userSlice = createSlice({
+    name: 'users',
+    initialState: {
+        list: []
+    },
+    reducers: {
+
+    }
+})
+
+export default userSlice.reducer
